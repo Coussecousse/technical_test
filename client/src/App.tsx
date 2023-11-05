7c207eb5469fe954bc3104c79eaefbce56f0125a
Erika <adob.e@hotmail.com>
2023-11-05 16:49:05 +0100

    feat: adding a test between back-end and front-end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface TestData {
+  message: string
+}
+
 function App() {
+
+  const [ data, setData ] = useState< string | undefined>(undefined)
+
+  useEffect(() => {
+    fetch('/test')
+    .then(res => res.json())
+    .then((data : TestData) => setData(data.message))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {data ? data : "No data"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
