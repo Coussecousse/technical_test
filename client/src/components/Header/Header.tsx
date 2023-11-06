4a7e44587a916e933218d9ea74187be62428f5d1
Erika <adob.e@hotmail.com>
2023-11-06 13:29:29 +0100

    feat: adding style to header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css"
+
+import paths from '../../config/paths'
 
 export default function Header() {
     return(
-        <header>
-            
+        <header className={styles.header}>
+            <nav className={styles.navContainer}>
+                <ul className={styles.linksContainer}>
+                    <li><NavLink to={paths.HOME}>Accueil</NavLink></li>
+                    <li><NavLink to={paths.GET_TICKET}>Se garer</NavLink></li>
+                    <li><NavLink to={paths.LEAVE_PARKING}>Quitter le parking</NavLink></li>
+                    <li><NavLink to={paths.SEE_PARKING}>Parking</NavLink></li>
+                </ul>
+            </nav>
+            <button className={styles.mobileButton}><span></span><span></span><span></span></button>
         </header>
     )
 }
