42166a0cffd80af53340e6b13a6b8f9f2d8c09d6
Erika <adob.e@hotmail.com>
2023-11-06 22:24:20 +0100

    feat:  adding home

--- client/src/containers/Home/Home.tsx
+++ client/src/containers/Home/Home.tsx
@@ -1,5 +1,21 @@
+import styles from './Home.module.css'
+
+import paths from "../../config/paths"
+
 export default function Home() {
     return (
-        <></>
+        <main >
+            <div className={styles.linksContainer}>
+                <a href={paths.GET_TICKET} className={styles.link}>
+                    Prendre un ticket
+                </a>
+                <a href={paths.LEAVE_PARKING} className={styles.link}>
+                    Quitter le parking
+                </a>
+                <a href={paths.SEE_PARKING} className={styles.link}>
+                    Voir le parking
+                </a>
+            </div>
+        </main>
     )
 }
