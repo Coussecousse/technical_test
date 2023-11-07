ea33b2dc150f7fa0fa78ab02a33177274d911d44
Erika <adob.e@hotmail.com>
2023-11-07 11:43:18 +0100

    feat: style & html get ticket

--- client/src/containers/GetTicket/GetTicket.jsx
+++ client/src/containers/GetTicket/GetTicket.jsx
@@ -1,6 +1,16 @@
+import paths from '../../config/paths';
+import styles from './GetTicket.module.css';
+
 export default function GetTicket() {
     return (
-        <>
-        </>
+        <main>
+            <div className={styles.ticket}>
+                <h2 className={styles.important}><span>ID unique :</span> 12345</h2>
+                <p className={styles.important}><span>Place :</span> 23</p>
+                <p>Pensez à garder votre ID sous la main !</p>
+                <a href={paths.SEE_PARKING} className={styles.button}>Trouver sa place</a>
+                <a href={paths.SEE_PARKING} className={styles.button}>Changer de place</a>
+            </div>
+        </main>
     )
 }
