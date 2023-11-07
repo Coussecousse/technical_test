e4a71bb20937dfefc7aff91fb2eb57ac04f715a7
Erika <adob.e@hotmail.com>
2023-11-07 11:08:22 +0100

    bug : fix the header z-index

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,32 +3,51 @@ import styles from "./Header.module.css";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import paths from '../../config/paths'
+import paths from "../../config/paths";
 import { selectors, actionTypes } from "../../redux/reducers/menu";
 
 export default function Header() {
-    const isMenuOpen = useSelector(selectors.getMenuValue);
-    const dispatch = useDispatch();
+  const isMenuOpen = useSelector(selectors.getMenuValue);
+  const dispatch = useDispatch();
 
-    const handleMenu = () => {
-        if (isMenuOpen) {
-            dispatch({ type : actionTypes.CLOSE_MENU })
-        } else {
-            dispatch({ type : actionTypes.OPEN_MENU})
-        }
+  const handleMenu = () => {
+    if (isMenuOpen) {
+      dispatch({ type: actionTypes.CLOSE_MENU });
+    } else {
+      dispatch({ type: actionTypes.OPEN_MENU });
     }
+  };
 
-    return(
-        <header className={styles.header}>
-            <nav className={`${styles.navContainer} ${isMenuOpen ? styles.open : ''}`}>
-                <ul className={styles.linksContainer}>
-                    <li><NavLink to={paths.HOME}>Accueil</NavLink></li>
-                    <li><NavLink to={paths.GET_TICKET}>Se garer</NavLink></li>
-                    <li><NavLink to={paths.LEAVE_PARKING}>Quitter le parking</NavLink></li>
-                    <li><NavLink to={paths.SEE_PARKING}>Voir le parking</NavLink></li>
-                </ul>
-            </nav>
-            <button className={`${styles.mobileButton} ${isMenuOpen ? styles.open : '' }`} onClick={handleMenu}><span></span><span></span><span></span></button>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+        <nav
+          className={`${styles.navContainer} ${isMenuOpen ? styles.open : ""}`}
+        >
+          <ul className={styles.linksContainer}>
+            <li>
+              <NavLink to={paths.HOME}>Accueil</NavLink>
+            </li>
+            <li>
+              <NavLink to={paths.GET_TICKET}>Se garer</NavLink>
+            </li>
+            <li>
+              <NavLink to={paths.LEAVE_PARKING}>Quitter le parking</NavLink>
+            </li>
+            <li>
+              <NavLink to={paths.SEE_PARKING}>Voir le parking</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.buttonContainer}>
+        <button
+          className={`${styles.mobileButton} ${isMenuOpen ? styles.open : ""}`}
+          onClick={handleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+  );
 }
