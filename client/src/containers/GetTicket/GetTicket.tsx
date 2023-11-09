cd0fa25e6d452e6359118d832d7fd27db88e928d
Erika <adob.e@hotmail.com>
2023-11-09 13:51:48 +0100

    feat: update parking

--- client/src/containers/GetTicket/GetTicket.tsx
+++ client/src/containers/GetTicket/GetTicket.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import styles from "./GetTicket.module.css";
 
 import paths from "../../config/paths";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store";
+import { updatePlaces } from "../../redux/reducers/places/actions/placesActions";
 
 interface Data {
   unique_id: string,
@@ -12,18 +15,23 @@ export default function GetTicket() {
 
   const [data, setData] = useState<Data | null>(null);
   const [error, setError] = useState<Boolean>(false);
+  const dispatch = useDispatch<AppDispatch>();
 
 
   useEffect(() => {
     if (data) return;
     fetch(paths.GET_TICKET_API)
     .then(res => res.json())
-    .then(data => setData(data))
+    .then(data => {
+      setData(data)
+      dispatch(updatePlaces()) 
+    })
     .catch(err => {
       console.log(err);
       setError(true)
     }
     );
+
   }, []);
 
   return (
