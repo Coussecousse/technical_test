b1ac561c2dccf1f0bd9e6a24bffb05da1e33299b
Erika <adob.e@hotmail.com>
2023-11-09 21:57:37 +0100

    feat: adding little car update on places

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
+import { selectors, actionTypes } from "../../redux/reducers/menu";
+import { selectors as placesSelectors } from "../../redux/reducers/places";
 
 import { useDispatch, useSelector } from "react-redux";
 
 import paths from "../../config/paths";
-import { selectors, actionTypes } from "../../redux/reducers/menu";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const parking: any = useSelector(placesSelectors.getPlacesValue);
+  const [ freePlaces, setFreePlaces ] = useState<number>(0);
+
+  useEffect(() => {
+    const places = parking.places.filter((place: any) => !place.occupied);
+    setFreePlaces(places.length);
+  }, [parking]);
+
   const isMenuOpen = useSelector(selectors.getMenuValue);
   const dispatch = useDispatch();
 
@@ -20,6 +30,7 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
+        <a href={paths.SEE_PARKING} className={styles.car}><i className="fa-solid fa-car"></i><span>{freePlaces}</span></a>
         <nav
           className={`${styles.navContainer} ${isMenuOpen ? styles.open : ""}`}
         >
